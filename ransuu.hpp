0cae37e341b8e4065ebcb8ce918784524ab628d9
LeeEndl <gavinolson71@gmail.com>
2024-09-13 17:02:30 -0500

    fun usage of struct rather than array.

--- ransuu.hpp
+++ ransuu.hpp
@@ -1,14 +1,19 @@
-#include <array>
 #include <time.h>
+
+struct shorts
+{
+    short front{};
+    short back{};
+};
 
 class ransuu
 {
-    time_t m{1};
-    public:
-    short operator[](std::array<short, 2> range) 
+    unsigned char m{1};
+public:
+    short operator[](shorts range) 
     {
-        short val = static_cast<short>(time(nullptr) % (++m));
+        short val = static_cast<short>(_time64(nullptr) % (++m));
         (val < (-0x7fff - 1)) ? val = (-0x7fff - 1) : (0x7fff < val) ? val = 0x7fff : val;
-        return val % (range.back() - range.front() + 1) + range.front();
+        return val % (range.back - range.front + 1) + range.front;
     }
 };
