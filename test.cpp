941afce5c237d0e8907b7bffb28e4b2cb331fa96
LeeEndl <gavinolson71@gmail.com>
2024-09-13 18:07:58 -0500

    randomize test (workflow)

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "ransuu.hpp"
+#include <stdio.h>
+
+int main()
+{
+    ransuu r{};
+    for (unsigned char i = 0; i < 50; ++i) 
+        printf("%d\n", r[shorts{0, 20}]);
+
+    return 0;
+}
