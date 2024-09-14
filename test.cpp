aae464a8c6b5c3dd0514dbf4fc73028a11f33cca
LeeEndl <gavinolson71@gmail.com>
2024-09-13 21:44:47 -0500

    updated struct name

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 int main()
 {
     ransuu r{};
-    for (unsigned char i = 0; i < 50; ++i) 
-        printf("%d\n", r[shorts{0, 20}]);
-
+    for (unsigned char i = 0; i < 255; ++i) // @note per ransuu class can only perform 255 random numbers.
+        printf("%d\n", r[range{0, 20}]);
+        
     return 0;
 }
