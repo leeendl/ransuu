72fefc05d98b6619d7ce0b0bdb7e94b50bf666e3
LeeEndl <gavinolson71@gmail.com>
2024-09-14 19:14:03 -0500

    ll_max/ll_min & minor retouch

--- ransuu.hpp
+++ ransuu.hpp
@@ -1,20 +1,23 @@
 #include <time.h>
 
+#define ll_max 9223372036854775807ll
+#define ll_min -9223372036854775807ll-1
+
 // @param front, back
 struct range
 {
     long long front{}, back{};
 };
 
-// @param operator[] provide range using range struct
 class ransuu
 {
     unsigned char m{0};
 public:
-    short operator[](range r) 
+    // @param _range provide a min, max range of numbers e.g. {0, 100}
+    long long operator[](range _range) 
     {
-        long long val = (_time64({}) ^ (++m << 4)) & 9223372036854775807ll-1;
-        (val < -9223372036854775807ll-1) ? val = -9223372036854775807ll-1 : (9223372036854775807ll < val) ? val = 9223372036854775807ll : val;
-        return val % (r.back - r.front + 1) + r.front;
+        long long val{ (_time64({}) ^ (++m << 4)) & ll_max };
+        (val < ll_min) ? val = ll_min : (ll_max < val) ? val = ll_max : val;
+        return{ val % (_range.back - _range.front + 1) + _range.front };
     }
 };
