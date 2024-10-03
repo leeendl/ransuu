e318cc42593d8222518503e0d9b9ef9d9514120e
LeeEndl <gavinolson71@gmail.com>
2024-10-03 17:08:06 -0500

    templates for dynamic integers

--- ransuu.hpp
+++ ransuu.hpp
@@ -5,9 +5,11 @@
 
 #if __cplusplus == 201103L
     #include <initializer_list>
-    using range = std::initializer_list<long long>;
+    template<typename T> 
+        using range = std::initializer_list<T>;
 #elif __cplusplus > 201103L
 // @param front, back
+template<typename T>
 struct range
 {
     long long front{}, back{};
@@ -19,20 +21,21 @@ class ransuu
     unsigned char m{0};
 public:
     // @param _range provide a min, max range of numbers e.g. {0, 100}
-    long long operator[](range _range) 
-    {
-        std::time_t t = std::time({});
-        long long val = (std::localtime(&t)->tm_sec * 1000000ll) ^ ((++m << 8) & ll_max);
-        (val < ll_min + 1) ? 
-            val = ll_min + 1 : 
-        (ll_max < val) ? 
-            val = ll_max : val;
-#if __cplusplus == 201103L
-        return{ val % (*(_range.begin() + 1) - *_range.begin() + 1) + *_range.begin() };
-#elif __cplusplus > 201103L
-        return{ val % (_range.back - _range.front + 1) + _range.front };
-#endif
-    };
+    template<typename T = long long>
+        T operator[](range<T> _range) 
+        {
+            std::time_t t = std::time({});
+            long long val = (std::localtime(&t)->tm_sec * 1000000ll) ^ ((++m << 8) & ll_max);
+            (val < ll_min + 1) ? 
+                val = ll_min + 1 : 
+            (ll_max < val) ? 
+                val = ll_max : val;
+            #if __cplusplus == 201103L
+                return static_cast<T>( val % (*(_range.begin() + 1) - *_range.begin() + 1) + *_range.begin() );
+            #elif __cplusplus > 201103L
+                return static_cast<T>( val % (_range.back - _range.front + 1) + _range.front );
+            #endif
+        };
 };
 
 #undef ll_max
