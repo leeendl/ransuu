00a5f5349d28da684a205e7e3f4d0b980e6236f8
LeeEndl <gavinolson71@gmail.com>
2024-10-13 21:11:46 -0500

    c++14 and above support `++` operator
    just a QoL commit making reading easier for modern C++ coders.

--- ransuu.hpp
+++ ransuu.hpp
@@ -1,4 +1,4 @@
-#include <pthread_time.h>
+#include <time.h>
 
 #define ll_max 9223372036854775807ll
 #define ll_min ll_max*(-1)
@@ -26,7 +26,7 @@ public:
         {
             struct timespec ts;
             clock_gettime(CLOCK_REALTIME, &ts);
-            long long val = ts.tv_nsec ^ ((++m << 8) | (ts.tv_nsec >> 5));
+            long long val = ts.tv_nsec ^ ( (++m << 8) | (ts.tv_nsec >> 5) );
             (val < ll_min) ? 
                 val = ll_min : 
             (ll_max < val) ? 
@@ -34,7 +34,7 @@ public:
             #if __cplusplus == 201103L
                 return static_cast<T>( val % (*(_range.begin() + 1) - *_range.begin() + 1) + *_range.begin() );
             #elif __cplusplus > 201103L
-                return static_cast<T>( val % (_range.back - _range.front + 1) + _range.front );
+                return static_cast<T>( val % ++(_range.back - _range.front) + _range.front );
             #endif
         };
 };
