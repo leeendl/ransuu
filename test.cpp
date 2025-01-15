4d696187bf5504f867263caaf70d1e8d31846590
LeeEndl <gavinolson71@gmail.com>
2025-01-15 17:04:02 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 int main()
 {
     ransuu r{};
-    for (unsigned char i = 0; i < 255; ++i) // @note per ransuu class can only perform 255 random numbers.
+    for (unsigned char i = 0; i < 50; ++i)
         printf("%d\n", r[{0, 100}]);
-    
+        
     return 0;
 }
