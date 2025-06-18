4f4d892c23d13a482b09b1a0ef60a02193a58669
leeendl <gavinolson71@gmail.com>
2025-06-18 15:02:42 -0500

    downsize long long to long

--- ransuu.hpp
+++ ransuu.hpp
@@ -1,14 +1,13 @@
 #include <pthread_time.h>
 
-#define ll_max 9223372036854775807ll
-#define ll_min -ll_max+1
+#define l_max 2147483647l
+#define l_min -l_max+1
 
 #if __cplusplus == 201103L
     #include <initializer_list>
     template<typename T> 
         using range = std::initializer_list<T>;
 #elif __cplusplus > 201103L
-// @param front, back
 template<typename T>
 struct range
 {
@@ -19,15 +18,15 @@ struct range
 class ransuu
 {
 public:
-    // @param _range provide a min, max range of numbers e.g. {0, 100}
-    template<typename T = long long>
+    // @param _range min, max range of numbers e.g. {0, 100}
+    template<typename T = long>
         T operator[](range<T> _range) 
         {
             struct timespec ts;
             clock_gettime(CLOCK_REALTIME, &ts);
-            long long val = ts.tv_nsec ^ ( 123 | (ts.tv_nsec >> 5) );
-            (val < ll_min) ? val = ll_min : 
-            (ll_max < val) ? val = ll_max : val;
+            long val = ts.tv_nsec ^ ( 123 | (ts.tv_nsec >> 5) );
+            (val < l_min) ? val = l_min : 
+            (l_max < val) ? val = l_max : val;
             #if __cplusplus == 201103L
                 return static_cast<T>( val % (*(_range.begin() + 1) - *_range.begin() + 1) + *_range.begin() );
             #elif __cplusplus > 201103L
@@ -36,5 +35,5 @@ public:
         };
 };
 
-#undef ll_max
-#undef ll_min
+#undef l_max
+#undef l_min
