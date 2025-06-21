484e4ba05b7c373db4264a966c5e50208dd2bcf7
leeendl <127639272+leeendl@users.noreply.github.com>
2025-06-20 19:58:21 -0500

    add floats

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
-#include "ransuu.hpp"
+#include "include/ransuu.hpp"
 #include <stdio.h>
 
 int main()
 {
-    ransuu r{};
+    ransuu ransuu{};
     for (unsigned char i = 0; i < 50; ++i)
-        printf("%d\n", r[{0, 100}]);
-        
+        printf("%d\n", ransuu[{0, 100}]);
+
+
     return 0;
 }
